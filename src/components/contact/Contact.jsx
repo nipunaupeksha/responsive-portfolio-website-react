783323a930fe2ab28771822d87883c8e5da894eb
nipunaupeksha <nipuna.upeksha@gmail.com>
2022-05-16 13:09:08 +0530

    files added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,61 @@
 import React from "react";
 import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessangerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
   return (
-    <section className="contact">Contact</section>
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon"/>
+            <h4>Email</h4>
+            <h5>iamflt@gmail.com</h5>
+            <a href="mailto:iamflt@gmail.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <RiMessangerLine className="contact__option-icon"/>
+            <h4>Messenger</h4>
+            <h5>@emmatouka</h5>
+            <a href="" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon"/>
+            <h4>Whatsapp</h4>
+            <h5>Emma Touka</h5>
+            <a href="" target="_blank">
+              Send a message
+            </a>
+          </article>
+        </div>
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
   );
 };
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import "./testimonials.css";
-import AVTR1 from '../../assets/avatar1.jpg';
-import AVTR2 from '../../assets/avatar2.jpg';
-import AVTR3 from '../../assets/avatar3.jpg';
-import AVTR4 from '../../assets/avatar4.jpg';
+import AVTR1 from "../../assets/avatar1.jpg";
+import AVTR2 from "../../assets/avatar2.jpg";
+import AVTR3 from "../../assets/avatar3.jpg";
+import AVTR4 from "../../assets/avatar4.jpg";
+
+import { Pagination } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 const Testimonials = () => {
   return (
@@ -11,53 +20,67 @@ const Testimonials = () => {
       <h5>Review from cleints</h5>
       <h2>Testimonials</h2>
       <div className="container testimonials__container">
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVATR1} alt="" />
-            <h5 className="client__name">Ernest Achiever</h5>
-            <small className="client__review">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Nulla hendrerit non mauris sit amet blandit. Aenean eu nisi viverra, pharetra ipsum ac, facilisis leo. 
-            Suspendisse potenti.
-            </small>
-          </div>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVATR2} alt="" />
-            <h5 className="client__name">Ernest Achiever</h5>
-            <small className="client__review">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Nulla hendrerit non mauris sit amet blandit. Aenean eu nisi viverra, pharetra ipsum ac, facilisis leo. 
-            Suspendisse potenti.
-            </small>
-          </div>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVATR3} alt="" />
-            <h5 className="client__name">Ernest Achiever</h5>
-            <small className="client__review">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Nulla hendrerit non mauris sit amet blandit. Aenean eu nisi viverra, pharetra ipsum ac, facilisis leo. 
-            Suspendisse potenti.
-            </small>
-          </div>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVATR4} alt="" />
-            <h5 className="client__name">Ernest Achiever</h5>
-            <small className="client__review">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Nulla hendrerit non mauris sit amet blandit. Aenean eu nisi viverra, pharetra ipsum ac, facilisis leo. 
-            Suspendisse potenti.
-            </small>
-          </div>
-        </article>
+        <Swiper
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{clickable:true}}>
+          <SwiperSlide>
+            <article className="testimonial">
+              <div className="client__avatar">
+                <img src={AVTR1} alt="" />
+              </div>
+              <h5 className="client__name">Ernest Achiever</h5>
+              <small className="client__review">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                hendrerit non mauris sit amet blandit. Aenean eu nisi viverra,
+                pharetra ipsum ac, facilisis leo. Suspendisse potenti.
+              </small>
+            </article>
+          </SwiperSlide>
+          <SwiperSlide>
+            <article className="testimonial">
+              <div className="client__avatar">
+                <img src={AVTR2} alt="" />
+              </div>
+              <h5 className="client__name">Ernest Achiever</h5>
+              <small className="client__review">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                hendrerit non mauris sit amet blandit. Aenean eu nisi viverra,
+                pharetra ipsum ac, facilisis leo. Suspendisse potenti.
+              </small>
+            </article>
+          </SwiperSlide>
+          <SwiperSlide>
+            <article className="testimonial">
+              <div className="client__avatar">
+                <img src={AVTR3} alt="" />
+              </div>
+              <h5 className="client__name">Ernest Achiever</h5>
+              <small className="client__review">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                hendrerit non mauris sit amet blandit. Aenean eu nisi viverra,
+                pharetra ipsum ac, facilisis leo. Suspendisse potenti.
+              </small>
+            </article>
+          </SwiperSlide>
+          <SwiperSlide>
+            <article className="testimonial">
+              <div className="client__avatar">
+                <img src={AVTR4} alt="" />
+              </div>
+              <h5 className="client__name">Ernest Achiever</h5>
+              <small className="client__review">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                hendrerit non mauris sit amet blandit. Aenean eu nisi viverra,
+                pharetra ipsum ac, facilisis leo. Suspendisse potenti.
+              </small>
+            </article>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </section>
-    );
+  );
 };
 
 export default Testimonials;
